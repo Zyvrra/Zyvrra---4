3328dfba93bacfe4856c9e7c0f1750e424b376c7
Zyvrra <xolanisinethemba9@gmail.com>
2026-06-09 20:58:22 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
-import FeedPage from "./feed/page";
-
 export default function Home() {
-  return <FeedPage />;
+  return (
+    <div style={{ color: "white", padding: 40 }}>
+      ZYVRRA TEST WORKS
+    </div>
+  );
 }
